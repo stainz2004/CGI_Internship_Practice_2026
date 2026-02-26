e8f3eac3837249a2966ded47a1421013b0001af4
stainz2004 <SanderR999@gmail.com>
2026-02-26 23:53:20 +0200

    Connect frontend to backend through axios. Test SeatingType endpoint.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import api from './services/api'
+
+interface SeatingType {
+  id: number
+  type: string
+}
 
 function App() {
+  const [seatingTypes, setSeatingTypes] = useState<SeatingType[]>([])
+
+  useEffect(() => {
+    api.get<SeatingType[]>('/seating-type')
+      .then(response => setSeatingTypes(response.data))
+      .catch(error => console.error(error))
+  }, [])
 
   return (
     <>
-
+      <p>Hello</p>
+      <ul>
+        {seatingTypes.map(seatingType => (
+          <li key={seatingType.id}>{seatingType.type}</li>
+        ))}
+      </ul>
     </>
   )
 }
